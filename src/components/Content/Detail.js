930b603567ff7dbda380e2f3c43157a9587c8bfb
김용휘(YongHwee Kim)/Text Analysis Service팀/SKP <yonghwee.kim@sk.com>
2016-09-23 18:33:37 +0900

    - add react modules  - setting react route.

---
+++ src/components/Content/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class ContentDetailComponent extends React.Component {
+    render() {
+        return (
+            <div>content list page..</div>
+        )
+    }
+}
+
+export default ContentDetailComponent;
---
+++ src/components/Content/List.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 1002720 on 2016. 9. 23..
+ */
+import React, { Component } from 'react';
+import './List.css';
+
+class ContentListComponent extends Component {
+    render() {
+        return (
+            <p className="App-intro">
+                ccc To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        );
+    }
+}
+
+export default ContentListComponent;
