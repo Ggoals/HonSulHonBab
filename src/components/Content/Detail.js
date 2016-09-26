343d813b381aeb91cba5378442d293398d735634
김용휘(YongHwee Kim)/Text Analysis Service팀/SKP <yonghwee.kim@sk.com>
2016-09-26 18:30:51 +0900

    set publishing

--- src/components/Content/Detail.js
+++ src/components/Content/Detail.js
@@ -1,10 +1,116 @@
 import React from 'react';
-
+import './detail.css';
+import '../../../resources/images/banner-bg.jpg'
 
 class ContentDetailComponent extends React.Component {
     render() {
         return (
-            <div>content list page..</div>
+            <div>
+                <div className="content-section-a">
+
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-5 col-sm-6">
+                                <hr className="section-heading-spacer"/>
+                                <div className="clearfix"></div>
+                                <h2 className="section-heading">Death to the Stock Photo:<br/>Special Thanks</h2>
+                                <p className="lead">
+                                    <span>A special thanks to</span>
+                                    <a>Death to the Stock Photo</a>
+                                    <span>for providing the photographs that you see in this template. Visit their website to become a member.</span>
+                                </p>
+                            </div>
+                            <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                                <img className="img-responsive" src="img/ipad.png" alt=""/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="content-section-b">
+
+                    <div className="container">
+
+                        <div className="row">
+                            <div className="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                                <hr className="section-heading-spacer"/>
+                                <div className="clearfix"></div>
+                                <h2 className="section-heading">3D Device Mockups<br/>by PSDCovers</h2>
+                                <p className="lead">Turn your 2D designs into high quality, 3D product shots in
+                                    seconds using free Photoshop actions by <a target="_blank"
+                                                                               href="http://www.psdcovers.com/">PSDCovers</a>!
+                                    Visit their website to download some of their awesome, free photoshop actions!
+                                </p>
+                            </div>
+                            <div className="col-lg-5 col-sm-pull-6  col-sm-6">
+                                <img className="img-responsive" src="img/dog.png" alt=""/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="content-section-a">
+
+                    <div className="container">
+
+                        <div className="row">
+                            <div className="col-lg-5 col-sm-6">
+                                <hr className="section-heading-spacer"/>
+                                <div className="clearfix"></div>
+                                <h2 className="section-heading">Google Web Fonts and<br/>Font Awesome Icons</h2>
+                                <p className="lead">This template features the 'Lato' font, part of the <a
+                                    target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>,
+                                    as well as <a target="_blank" href="http://fontawesome.io">icons from Font
+                                        Awesome</a>.</p>
+                            </div>
+                            <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                                <img className="img-responsive" src="img/phones.png" alt=""/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="banner">
+
+                    <div className="container">
+
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <h2>Connect to Start Bootstrap:</h2>
+                            </div>
+                            <div className="col-lg-6">
+                                <ul className="list-inline banner-social-buttons">
+                                    <li>
+                                        <a href="https://twitter.com/SBootstrap" className="btn btn-default btn-lg">
+                                            <i className="fa fa-twitter fa-fw"></i>
+                                            <span className="network-name">Twitter</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://github.com/IronSummitMedia/startbootstrap"
+                                           className="btn btn-default btn-lg">
+                                            <i className="fa fa-github fa-fw"></i>
+                                            <span className="network-name">Github</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="btn btn-default btn-lg">
+                                            <i className="fa fa-linkedin fa-fw"></i>
+                                            <span className="network-name">Linkedin</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
         )
     }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,36 @@
  * Created by 1002720 on 2016. 9. 23..
  */
 import React from 'react';
-import './Header.css';
-import logo from '../../logo.svg';
 
 class HeaderComponent extends React.Component {
     render() {
         return (
-            <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-            </div>
+            <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">Start Bootstrap</a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li>
+                                <a href="#">Services</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         )
 
     }
